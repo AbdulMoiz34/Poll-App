eefd8d75676d0609d10ba23663b78c702f80fdca
Hassan Ali <hassanali20242025@gmail.com>
2025-04-16 23:57:50 +0500

    add this login and signup page abdul moize

---
+++ app.js
@@ -0,0 +1,22 @@
+let button = document.getElementById("signupbutton");
+let arry = JSON.parse(localStorage.getItem("users")) || [];
+
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    console.log(email, password);
+
+    let user = {
+        email: email,
+        password: password
+    };
+
+    arry.push(user);
+    localStorage.setItem("users", JSON.stringify(arry));
+    window.location.href = "../loginpage.html";
+    alert("Signup successful!");
+    
+});
